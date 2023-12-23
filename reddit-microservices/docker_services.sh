8ff5b436746f7ce68979fb792a85142ff035537d
Vladimir <vladimir_tmail@mail.ru>
2023-12-23 17:30:00 +0000

    Moved directory src

--- reddit-microservices/docker_services.sh
+++
@@ -1,5 +0,0 @@
-docker kill $(docker ps -q)
-docker run -d --network=reddit --network-alias=post_db --network-alias=comment_db -v reddit_db:/data/db  mongo:4
-docker run -d --network=reddit --network-alias=post wowan/post:1.0
-docker run -d --network=reddit --network-alias=comment wowan/comment:1.0
-docker run -d --network=reddit -p 9292:9292 wowan/ui:2.0
