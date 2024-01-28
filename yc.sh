a06ba364d0408a8bd7c7dc5b968147cb901f6056
Vladimir <vladimir_tmail@mail.ru>
2024-01-28 17:34:58 +0000

    Kubernetes init

--- yc.sh
+++ yc.sh
@@ -1,8 +1,8 @@
 yc compute instance create \
-  --name docker-host \
-  --zone ru-central1-a \
-  --network-interface subnet-name=default-ru-central1-a,nat-ip-version=ipv4 \
-  --create-boot-disk image-folder-id=standard-images,image-family=ubuntu-1804-lts,size=15 \
+  --name works-node \
+  --zone ru-central1-b \
+  --network-interface subnet-name=default-ru-central1-b,nat-ip-version=ipv4 \
+  --create-boot-disk image-folder-id=standard-images,image-family=ubuntu-1804-lts,size=40 \
   --memory=4G \
-  --cores=2 \
+  --cores=4 \
   --ssh-key ~/.ssh/id_rsa.pub
